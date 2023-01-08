24a0040dac7257e5c8e201ea81b2b75d778e2c74
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2023-01-08 14:48:58 +0600

    Start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Options/Css/index.css';
+import App from './UWMComponents/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
