7f168a9b1231136026ae6135ca59e4f3c53556fe
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2023-01-12 13:20:06 +0600

    Specifications in table view, New NavaBar

--- src/UWMComponents/ItemsComponents/ItemDetail.js
+++ src/UWMComponents/ItemsComponents/ItemDetail.js
@@ -37,43 +37,58 @@ class Item extends Component {
     if (typeof item.specifications === 'string')
       return (
         <div >
-          <div className="row ">
-            <div className="mx-auto col-md-11 card border-dark" >
-              <div className="card-header">{item.title}</div>
-              <div className="card-body text-dark">
-                <table className="table table-hover">
-                  <tbody>
-                    <tr>
-                      <th>Издатель</th>
-                      <th>{item.manufacturer}</th>
+          <br /><br />
+          <div className="mx-auto col-md-11 card border-dark" >
+            <div className="card-header bg-transparent border-dark"><h3>{item.title}</h3></div>
+            <div className="card-body text-dark">
+              <table className="table table-hover">
+                <tbody>
+                  <tr>
+                    <th>Издатель</th>
+                    <th>{item.manufacturer}</th>
+                  </tr>
+                  <tr>
+                    <th>Поставщик</th>
+                    <th>{item.providerName}</th>
+                  </tr>
+                  <tr>
+                    <th>Количество</th>
+                    <th>{item.quantity}/{item.unit}</th>
+                  </tr>
+                  <tr>
+                    <th>Категория</th>
+                    <th>{item.subCategoryName}</th>
+                  </tr>
+                  <tr>
+                    <th>Номер склада</th>
+                    <th>{item.warehouseNumber}</th>
+                  </tr>
+                </tbody>
+              </table>
+              <br />
+              <h4>Характеристики</h4>
+              <br />
+              <table className="table table-hover">
+                <tbody>
+                  {item.specifications.split(";").map((x, i) =>
+                    <tr key={i}>
+                      {x.split(":").map((e, i) =>
+                        <th key={i}>{e}</th>)}
                     </tr>
-                    <tr>
-                      <th>Поставщик</th>
-                      <th>{item.providerName}</th>
-                    </tr>
-                    <tr>
-                      <th>Количество</th>
-                      <th>{item.quantity}/{item.unit}</th>
-                    </tr>
-                  </tbody>
-                </table>
-                <br />
-                <h4>Характеристики</h4>
-                <br />
-                <p className="card-text">
-                  <table className="table table-hover">
-                    <tbody>
-                      {item.specifications.split(";").map(x =>
-                        <tr>
-                          <th>{x.split("-")[0]} </th>
-                          <th>{x.split("-")[1]} </th>
-                        </tr>
-                      )}
-                    </tbody>
-                  </table>
-                </p>
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <div className="card-footer border-dark bg-transparent">
+              <div className='row '>
+                <div class="col-8 d-grid gap-2 d-md-flex">
+                  <button type="button" class="btn btn-outline-dark fw-bolder">Назад</button>
+                  <button type="button" class="btn btn-outline-warning fw-bolder">Редактировать</button>
+                </div>
+                <div class="col-4 d-grid gap-2 d-md-flex justify-content-end">
+                  <button type="button" class="btn btn-outline-danger fw-bolder">Удалить</button>
+                </div>
               </div>
-              <div className="card-footer border-dark">Footer</div>
             </div>
           </div>
         </div>
@@ -81,4 +96,4 @@ class Item extends Component {
   }
 }
 // Запись характеристик
-// название характеристики - Характеристика; След характеристика - характеристика
+// название характеристики - Характеристика; След характеристика - характеристика 
--- src/UWMComponents/ItemsComponents/NavbarComponent.js
+++ src/UWMComponents/ItemsComponents/NavbarComponent.js
@@ -19,22 +19,28 @@ export default class NavbarComponent extends Component {
     render() {
         return (
             <div>
-                <div className="border-bottom">
-                    <nav className="navbar navbar-light">
-                        <div className="container-fluid">
-                            <a className="navbar-brand">Navbar</a>
-                            <Link to="/" >Home</Link>
-                            <Link to="/itemDetail/" >ID</Link>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">UWM</a>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="/">Список</Link>
+                                </li>
+                            </ul>
                             <form className="d-flex">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Search</button>
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <button className="btn btn-outline-success" type="submit">Search</button>
                             </form>
                         </div>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
                 <Routes>
-                    <Route path="/" element={<ListItem />}/>
-                    <Route path="/itemDetail/:id" element={<ItemDetail />}/>
+                    <Route path="/" element={<ListItem />} />
+                    <Route path="/itemDetail/:id" element={<ItemDetail />} />
                 </Routes>
             </div>
         )
