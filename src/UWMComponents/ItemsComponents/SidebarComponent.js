ddfa421b7c06277a50a8f6f4ec656e2e18706172
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2023-01-13 17:15:06 +0600

    sidebar fix

--- src/UWMComponents/ItemsComponents/SidebarComponent.js
+++ src/UWMComponents/ItemsComponents/SidebarComponent.js
@@ -5,34 +5,42 @@ export default class SidebarComponent extends Component {
     constructor(props) {
         super(props);
         this.categoryServices = new CategoryServices();
-        
+
         this.state = {
             categorys: [],
             subcategoryid: 0,
         }
-    }  
+    }
 
     async componentDidMount() {
-        this.setState({ categorys: await this.categoryServices.getCategory() }); 
+        this.setState({ categorys: await this.categoryServices.getCategory() });
     }
 
 
     render() {
+        console.log(this.state.categorys)
         return (
             <div>
-                <div>{this.props.subcategoryid}</div>
-                {this.state.categorys.map((category) =>
-                    <div key={category.id} className="col-md-10">
+                {this.state.categorys.map(category =>
+                    <div key={category.id} className="accordion" id={category.name.replace(" ", "") + "Wrapper"}>
                         <br />
-                        <button className="btn btn-outline-dark rounded colapsed col-12" type="button" data-bs-toggle="collapse" data-bs-target={"#" + category.name.replace(" ", "")} aria-expanded="false" aria-controls={category.name.replace(" ", "")}>
-                            <h6 className="accordion-header">{category.name}</h6>
-                        </button>
-                        <div id={category.name.replace(" ", "")} className="accordion-collapse collapse" aria-labelledby={category.name.replace(" ", "")} data-bs-parent="#accordionExample">
-                            <div className="accordion-body ">
-                                <div className="list-group rounded">
-                                    {category.subCategoryDto.map(subcategory =>
-                                        <button key={subcategory.id} value={subcategory.id} onClick={() => this.props.getBy(subcategory.id)} type="button" className="btn btn-outline-secondary">{subcategory.name}</button>
-                                    )}
+                        <div className="accordion-item">
+                            <button className="btn btn-outline-dark rounded colapsed col-12"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target={"#" + category.name.replace(" ", "")}
+                                aria-expanded="true"
+                                aria-controls={category.name.replace(" ", "")}>
+                                {category.name}
+                            </button>
+                            <div id={category.name.replace(" ", "")} className="accordion-collapse collapse"
+                                data-bs-parent={"#" + category.name.replace(" ", "") + "Wrapper"}>
+                                <div className="accordion-body">
+                                    <div className="list-group rounded">
+                                        {category.subCategoryDto.map(subcategory =>
+                                            <button key={subcategory.id} value={subcategory.id} onClick={() => this.props.getBy(subcategory.id)} type="button" className="btn btn-outline-secondary">{subcategory.name}</button>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
                         </div>
