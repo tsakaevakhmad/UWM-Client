6ad3532a12b6e95aba354c852e3096c1e674fbc2
ZLUKADARK <ahmadcakaev@mail.ru>
2023-01-18 19:26:16 +0600

    loading pages ++

--- src/Components/ItemsComponents/ItemEdit.js
+++ src/Components/ItemsComponents/ItemEdit.js
@@ -86,8 +86,7 @@ class Item extends Component {
     }
 
     async componentDidMount() {
-        let item = {};
-        item = await this.itemServices.getItem(this.props.itemId)
+        const item = await this.itemServices.getItem(this.props.itemId)
         this.setState({
             item: item,
             id: item.id,
@@ -112,111 +111,51 @@ class Item extends Component {
         if (this.state.redirectToList) {
             return <Navigate to={"/"} />
         }
-
-        if (edit) {
-            return (
-                <div >
-                    <br /><br />
-                    <div className="mx-auto col-md-11 card border-dark" >
-                        <div className="card-header bg-transparent border-dark"><h3>{this.state.title}</h3></div>
-                        <div className="card-body text-dark">
-                            <label className="form-label">Название</label>
-                            <input className="form-control" type="text" value={title} name="title" onChange={this.handleChange} placeholder="Название" />
-                            <label className="form-label">Производитель</label>
-                            <input className="form-control" type="text" value={manufacturer} name="manufacturer" onChange={this.handleChange} placeholder="Производитель" />
-                            <label className="form-label">Цена</label>
-                            <input className="form-control" type="number" value={price} name="price" onChange={this.handleChange} placeholder="Цена" />
-                            <label className="form-label">Количество / единица измерения</label>
-                            <div className="input-group ">
-                                <input className="form-control col-6" type="number" value={quantity} name="quantity" onChange={this.handleChange} placeholder="Количество" />
-                                <input className="form-control col-6" type="text" value={unit} name="unit" onChange={this.handleChange} placeholder="Единица" />
-                            </div>
-                            <label className="form-label">Поставщик</label>
-                            <select className="form-select" name="providerId" value={providerId} onChange={this.handleChange}>
-                                {this.state.provider.map(p =>
-                                    <option key={p.id} value={p.id}>{p.name}</option>
-                                )}
-                            </select>
-                            <label className="form-label">Категория</label>
-                            <select className="form-select" name="subCategoryId" value={subCategoryId} onChange={this.handleChange}>
-                                {this.state.subCategory.map(sc =>
-                                    <option key={sc.id} value={sc.id}>{sc.name}</option>
-                                )}
-                            </select>
-                            <label className="form-label">Склад</label>
-                            <select className="form-select" name="warehouseId" value={warehouseId} onChange={this.handleChange}>
-                                {this.state.wahehouse.map(w =>
-                                    <option key={w.id} value={w.id}>{w.number}</option>
-                                )}
-                            </select>
-                            <label className="form-label">Описание товара</label>
-                            <textarea className="form-control" name="specifications" value={specifications} onChange={this.handleChange} rows="3"></textarea>
-                        </div>
-                        <div className="card-footer border-dark bg-transparent">
-                            <div className="row" >
-                                <div className="col-8 d-grid gap-2 d-md-flex">
-                                    <button type="button" onClick={() => this.setState({ edit: false })} className="btn btn-outline-dark fw-bolder">Отмена</button>
-                                    <button type="button" onClick={this.Update} className="btn btn-outline-success fw-bolder">Сохранить</button>
-                                </div>
-                                <div className="col-4 d-grid gap-2 d-md-flex justify-content-end">
-                                    <button type="button" onClick={this.Delete} className="btn btn-outline-danger fw-bolder">Удалить</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )
-        } else {
-            if (typeof item.specifications === 'string')
+        if (item != null)
+            if (edit) {
                 return (
                     <div >
                         <br /><br />
                         <div className="mx-auto col-md-11 card border-dark" >
-                            <div className="card-header bg-transparent border-dark"><h3>{item.title}</h3></div>
+                            <div className="card-header bg-transparent border-dark"><h3>{this.state.title}</h3></div>
                             <div className="card-body text-dark">
-                                <table className="table table-hover">
-                                    <tbody>
-                                        <tr>
-                                            <th>Издатель</th>
-                                            <th>{item.manufacturer}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Поставщик</th>
-                                            <th>{item.providerName}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Количество</th>
-                                            <th>{item.quantity}/{item.unit}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Категория</th>
-                                            <th>{item.subCategoryName}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Номер склада</th>
-                                            <th><Link to={`/WarehouseEdit/${item.warehouseId}`}>{item.warehouseNumber}</Link></th>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <br />
-                                <h4>Характеристики</h4>
-                                <br />
-                                <table className="table table-hover">
-                                    <tbody>
-                                        {item.specifications.split(";").map((x, i) =>
-                                            <tr key={i}>
-                                                {x.split(":").map((e, i) =>
-                                                    <th key={i}>{e}</th>)}
-                                            </tr>
-                                        )}
-                                    </tbody>
-                                </table>
+                                <label className="form-label">Название</label>
+                                <input className="form-control" type="text" value={title} name="title" onChange={this.handleChange} placeholder="Название" />
+                                <label className="form-label">Производитель</label>
+                                <input className="form-control" type="text" value={manufacturer} name="manufacturer" onChange={this.handleChange} placeholder="Производитель" />
+                                <label className="form-label">Цена</label>
+                                <input className="form-control" type="number" value={price} name="price" onChange={this.handleChange} placeholder="Цена" />
+                                <label className="form-label">Количество / единица измерения</label>
+                                <div className="input-group ">
+                                    <input className="form-control col-6" type="number" value={quantity} name="quantity" onChange={this.handleChange} placeholder="Количество" />
+                                    <input className="form-control col-6" type="text" value={unit} name="unit" onChange={this.handleChange} placeholder="Единица" />
+                                </div>
+                                <label className="form-label">Поставщик</label>
+                                <select className="form-select" name="providerId" value={providerId} onChange={this.handleChange}>
+                                    {this.state.provider.map(p =>
+                                        <option key={p.id} value={p.id}>{p.name}</option>
+                                    )}
+                                </select>
+                                <label className="form-label">Категория</label>
+                                <select className="form-select" name="subCategoryId" value={subCategoryId} onChange={this.handleChange}>
+                                    {this.state.subCategory.map(sc =>
+                                        <option key={sc.id} value={sc.id}>{sc.name}</option>
+                                    )}
+                                </select>
+                                <label className="form-label">Склад</label>
+                                <select className="form-select" name="warehouseId" value={warehouseId} onChange={this.handleChange}>
+                                    {this.state.wahehouse.map(w =>
+                                        <option key={w.id} value={w.id}>{w.number}</option>
+                                    )}
+                                </select>
+                                <label className="form-label">Описание товара</label>
+                                <textarea className="form-control" name="specifications" value={specifications} onChange={this.handleChange} rows="3"></textarea>
                             </div>
                             <div className="card-footer border-dark bg-transparent">
                                 <div className="row" >
                                     <div className="col-8 d-grid gap-2 d-md-flex">
-                                        <Link className="btn btn-outline-dark fw-bolder" to="/">Назад</Link>
-                                        <button type="button" onClick={() => this.setState({ edit: true })} className="btn btn-outline-warning fw-bolder">Редактировать</button>
+                                        <button type="button" onClick={() => this.setState({ edit: false })} className="btn btn-outline-dark fw-bolder">Отмена</button>
+                                        <button type="button" onClick={this.Update} className="btn btn-outline-success fw-bolder">Сохранить</button>
                                     </div>
                                     <div className="col-4 d-grid gap-2 d-md-flex justify-content-end">
                                         <button type="button" onClick={this.Delete} className="btn btn-outline-danger fw-bolder">Удалить</button>
@@ -226,6 +165,76 @@ class Item extends Component {
                         </div>
                     </div>
                 )
-        }
+            } else {
+                if (typeof item.specifications === 'string')
+                    return (
+                        <div >
+                            <br /><br />
+                            <div className="mx-auto col-md-11 card border-dark" >
+                                <div className="card-header bg-transparent border-dark"><h3>{item.title}</h3></div>
+                                <div className="card-body text-dark">
+                                    <table className="table table-hover">
+                                        <tbody>
+                                            <tr>
+                                                <th>Издатель</th>
+                                                <th>{item.manufacturer}</th>
+                                            </tr>
+                                            <tr>
+                                                <th>Поставщик</th>
+                                                <th>{item.providerName}</th>
+                                            </tr>
+                                            <tr>
+                                                <th>Количество</th>
+                                                <th>{item.quantity}/{item.unit}</th>
+                                            </tr>
+                                            <tr>
+                                                <th>Категория</th>
+                                                <th>{item.subCategoryName}</th>
+                                            </tr>
+                                            <tr>
+                                                <th>Номер склада</th>
+                                                <th><Link to={`/WarehouseEdit/${item.warehouseId}`}>{item.warehouseNumber}</Link></th>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <br />
+                                    <h4>Характеристики</h4>
+                                    <br />
+                                    <table className="table table-hover">
+                                        <tbody>
+                                            {item.specifications.split(";").map((x, i) =>
+                                                <tr key={i}>
+                                                    {x.split(":").map((e, i) =>
+                                                        <th key={i}>{e}</th>)}
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div className="card-footer border-dark bg-transparent">
+                                    <div className="row" >
+                                        <div className="col-8 d-grid gap-2 d-md-flex">
+                                            <Link className="btn btn-outline-dark fw-bolder" to="/">Назад</Link>
+                                            <button type="button" onClick={() => this.setState({ edit: true })} className="btn btn-outline-warning fw-bolder">Редактировать</button>
+                                        </div>
+                                        <div className="col-4 d-grid gap-2 d-md-flex justify-content-end">
+                                            <button type="button" onClick={this.Delete} className="btn btn-outline-danger fw-bolder">Удалить</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+            }
+        return (
+            <div>
+                <br /><br />
+                <br />
+                <div className="text-center">
+                    <div className="spinner-border" role="status">
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
--- src/Components/ItemsComponents/Sidebar.js
+++ src/Components/ItemsComponents/Sidebar.js
@@ -18,33 +18,44 @@ export default class Sidebar extends Component {
 
 
     render() {
-        return (
-            <div>
-                {this.state.categorys.map(category =>
-                    <div key={category.id} className="accordion" id={category.name.replace(" ", "") + "Wrapper"}>
-                        <br />
-                        <div className="accordion-item">
-                            <button className="btn btn-outline-dark rounded colapsed col-12"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target={"#" + category.name.replace(" ", "")}
-                                aria-expanded="true"
-                                aria-controls={category.name.replace(" ", "")}>
-                                {category.name}
-                            </button>
-                            <div id={category.name.replace(" ", "")} className="accordion-collapse collapse"
-                                data-bs-parent={"#" + category.name.replace(" ", "") + "Wrapper"}>
-                                <div className="accordion-body">
-                                    <div className="list-group rounded">
-                                        {category.subCategoryDto.map(subcategory =>
-                                            <button key={subcategory.id} value={subcategory.id} onClick={() => this.props.getBy(subcategory.id)} type="button" className="btn btn-outline-secondary">{subcategory.name}</button>
-                                        )}
+        if (this.state.categorys != null)
+            return (
+                <div>
+                    {this.state.categorys.map(category =>
+                        <div key={category.id} className="accordion" id={category.name.replace(" ", "") + "Wrapper"}>
+                            <br />
+                            <div className="accordion-item">
+                                <button className="btn btn-outline-dark rounded colapsed col-12"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target={"#" + category.name.replace(" ", "")}
+                                    aria-expanded="true"
+                                    aria-controls={category.name.replace(" ", "")}>
+                                    {category.name}
+                                </button>
+                                <div id={category.name.replace(" ", "")} className="accordion-collapse collapse"
+                                    data-bs-parent={"#" + category.name.replace(" ", "") + "Wrapper"}>
+                                    <div className="accordion-body">
+                                        <div className="list-group rounded">
+                                            {category.subCategoryDto.map(subcategory =>
+                                                <button key={subcategory.id} value={subcategory.id} onClick={() => this.props.getBy(subcategory.id)} type="button" className="btn btn-outline-secondary">{subcategory.name}</button>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    )}
+                </div>
+            )
+        return (
+            <div>
+                <br/><br/>
+                <br/>
+                <div className="text-center">
+                    <div className="spinner-border" role="status">
                     </div>
-                )}
+                </div>
             </div>
         )
     }
